2753669575a1c0d6f17b06e5a86ead836b706908
Natan das Chagas Silva <natan.c.silva@uol.com.br>
2025-03-15 16:37:13 -0300

    feat: improve workflows, create more schedules, add lambda iam

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-module "custodian_eventbridge_role" {
+module "custodian_schedule_role" {
   source = "./iam"
 
   role = {
@@ -11,6 +11,20 @@ module "custodian_eventbridge_role" {
   policy = {
     name   = "cloud-custodian-policy"
     policy = templatefile("iam/policy-templates/scheduler-policy.json", {})
+  }
+}
+
+module "custodian_lambda_role" {
+  source = "./iam"
+
+  role = {
+    name         = "cloud-custodian-lambda-role"
+    trust_policy = templatefile("iam/trust-templates/lambda-trust-policy.json", {})
+  }
+
+  policy = {
+    name   = "cloud-custodian-lambda-policy"
+    policy = templatefile("iam/policy-templates/lambda-policy.json", {})
   }
 }
 
@@ -30,7 +44,7 @@ module "scheduler" {
   source = "./scheduler"
 
   schedule_group_name = aws_scheduler_schedule_group.cloud_custodian_schedule_group.name
-  schedule_role       = module.custodian_eventbridge_role.role_arn
+  schedule_role       = module.custodian_schedule_role.role_arn
 
   kms_key_arn = module.kms.kms_arn
 
--- infrastructure/scheduler/scheduler.tf
+++ infrastructure/scheduler/scheduler.tf
@@ -1,6 +1,65 @@
-resource "aws_scheduler_schedule" "cloud_custodian_schedule" {
-  name       = "cloud-custodian-schedule"
+resource "aws_scheduler_schedule" "cloud_custodian_schedule_0" {
+  name       = "cloud-custodian-schedule-0"
   group_name = var.schedule_group_name
+
+  description = "A schedule with KMS key, using a cron expression and a custom schedule group"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 0 20 12 ? *)"
+
+  kms_key_arn = var.kms_key_arn
+
+  target {
+    arn      = var.target_arn
+    role_arn = var.schedule_role
+  }
+}
+
+resource "aws_scheduler_schedule" "cloud_custodian_schedule_1" {
+  name       = "cloud-custodian-schedule-1"
+  group_name = var.schedule_group_name
+
+    description = "A schedule without KMS key, using a cron expression and a custom schedule group"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 0 20 12 ? *)"
+
+  target {
+    arn      = var.target_arn
+    role_arn = var.schedule_role
+  }
+}
+
+resource "aws_scheduler_schedule" "cloud_custodian_schedule_2" {
+  name       = "cloud-custodian-schedule-2"
+  group_name = var.schedule_group_name
+
+  description = "A schedule with KMS key, using a one time schedule expression and a custom schedule group"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "at(2025-12-20T00:00:00)"
+
+  kms_key_arn = var.kms_key_arn
+
+  target {
+    arn      = var.target_arn
+    role_arn = var.schedule_role
+  }
+}
+
+resource "aws_scheduler_schedule" "cloud_custodian_schedule_3" {
+  name       = "cloud-custodian-schedule-3"
+
+  description = "A schedule with KMS key, using a cron expression and a the default schedule group"
 
   flexible_time_window {
     mode = "OFF"
